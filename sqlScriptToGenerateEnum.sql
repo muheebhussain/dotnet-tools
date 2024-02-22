ee728995cbfd83c5f1fc63caea9717e0688619ec
muheebhussain <muheebhussain@live.com>
2024-02-22 12:20:53 -0500

    Update sqlScriptToGenerateEnum.sql

--- sqlScriptToGenerateEnum.sql
+++ sqlScriptToGenerateEnum.sql
@@ -23,3 +23,30 @@ SET @EnumTemplate = REPLACE(@EnumTemplate, '{0}', @EnumBody)
 
 -- Output the result
 PRINT @EnumTemplate
+
+
+-- Updated
+DECLARE @TableName NVARCHAR(128) = N'YourTableName'; -- Set your table name here
+DECLARE @EnumName NVARCHAR(128) = N'MyEnum'; -- Set your enum name here
+DECLARE @SQL NVARCHAR(MAX);
+DECLARE @EnumBody NVARCHAR(MAX) = '';
+DECLARE @EnumTemplate NVARCHAR(MAX) = 'public enum ' + @EnumName + ' {0}';
+
+-- Dynamic SQL to generate the enum body
+SET @SQL = N'SELECT @EnumBodyOUT = (SELECT ''['' + CAST(Id AS NVARCHAR) + ''] '' + 
+            Description + '' = '' + CAST(Id AS NVARCHAR) + '','' + CHAR(13)
+            FROM ' + QUOTENAME(@TableName) + 
+            ' ORDER BY Id FOR XML PATH(''''), TYPE).value(''.'', ''NVARCHAR(MAX)'')';
+
+-- Execute the dynamic SQL
+EXEC sp_executesql @SQL, N'@EnumBodyOUT NVARCHAR(MAX) OUTPUT', @EnumBody OUTPUT;
+
+-- Remove the last comma
+IF LEN(@EnumBody) > 0
+    SET @EnumBody = LEFT(@EnumBody, LEN(@EnumBody) - 2);
+
+-- Combine the template and the body
+SET @EnumTemplate = REPLACE(@EnumTemplate, '{0}', @EnumBody);
+
+-- Output the result
+PRINT @EnumTemplate;
